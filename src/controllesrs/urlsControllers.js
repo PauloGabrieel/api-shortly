51bc0a28cea71136aa4e65855c14eb1539b3bef2
PauloGabrieel <gabrieeel.c@gmail.com>
2022-08-06 15:44:51 -0300

    feat: redireciona o usuário para a url desejada

--- src/controllesrs/urlsControllers.js
+++ src/controllesrs/urlsControllers.js
@@ -16,11 +16,37 @@ export async function shortenUrl(req,res){
     };
 };
 export async function getShortUrlById(req, res){
-    const id = parseInt(req.params.id);
+    try {
+        const id = parseInt(req.params.id);
     
-    const data =  await connection.query('SELECT u.id, u.url, u."shortUrl" FROM urls u WHERE u.id=$1;',[id])
-    if(data.rowCount === 0){
-        return res.sendStatus(404);
+        const data =  await connection.query('SELECT u.id, u.url, u."shortUrl" FROM urls u WHERE u.id=$1;',[id])
+        if(data.rowCount === 0){
+            return res.sendStatus(404);
+        };
+        res.status(200).send(data.rows[0]);    
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("houve um erro na hora de buscar o link pelo id");
     };
-    res.status(200).send(data.rows[0]);
+    
+};
+
+export async function redirectUrl(req, res){
+    try {
+        const {shortUrl} = req.params;
+    
+        const data = await connection.query('SELECT * FROM urls WHERE "shortUrl"=$1',[shortUrl]);
+        if(data.rowCount === 0){
+            return res.sendStatus(404);
+        };
+        const {id} =data.rows[0]
+        const {visitCount} = data.rows[0];
+        const {url} =data.rows[0];
+        await connection.query('UPDATE urls SET "visitCount" = $1 WHERE id=$2',[(visitCount + 1),id])
+        res.redirect(url);    
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("houve um erro na hora de redirecionar para a url");
+    };
+    
 };
--- src/routes/urlsRoute.js
+++ src/routes/urlsRoute.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 
 import authentication from "../middlewares/tokenAuthentication.js";
 import { validateUrl } from "../middlewares/urlsValidator.js";
-import { shortenUrl,getShortUrlById } from "../controllesrs/urlsControllers.js";
+import { shortenUrl,getShortUrlById, redirectUrl } from "../controllesrs/urlsControllers.js";
 const route = Router();
 
 route.post("/urls/shorten",authentication,validateUrl,shortenUrl);
 route.get("/urls/:id",getShortUrlById);
+route.get("/urls/open/:shortUrl",redirectUrl);
 
 export default route;
